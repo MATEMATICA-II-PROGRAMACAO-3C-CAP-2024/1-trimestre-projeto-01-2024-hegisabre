28f48164c520abdf7fd431b50c8c2fb05841d333
hegisabre <130759660+hegisabre@users.noreply.github.com>
2024-03-27 08:54:09 -0300

    aula_6

--- main.js
+++ main.js
@@ -19,7 +19,14 @@ for(let i=0;i <botoes.length;i++){ botoes[i].onclick = function() {
     function calculadoraTempo(tempoObjetivo)
     let tempoAtual = new Date ()
     let tempoFinal= tempoObjetivo - tempoAtural;
-    let segundo = Math.floor (tempoFinal / 1000);
+    let segundos = Math.floor (tempoFinal / 1000);
     let minutos = Math.floor (segundos/60);
+    let horas = Math.floor (minutos/60);
+    let dias = Math.floor (horas/24);
+    segundos%=60; 
+    minutos%=60;
+    horas%24;
     
-    return segundos;
+
+    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + "segundos";
+
